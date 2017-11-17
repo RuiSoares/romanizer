b0431d67c99aa5a04fd437fa3dd415772dbd6281
RuiSoares <rui.ms1992@gmail.com>
2017-11-17 22:55:37 +0000

    Add hash with all roman symbols; Add romanize method;

--- lib/romanizer.rb
+++ lib/romanizer.rb
@@ -2,12 +2,43 @@ require 'errors'
 require 'pry'
 
 class Romanizer
+  ROMAN_NUMBERS = {
+    '_X' => 10000,
+    'M_X' => 9000,
+    '_V' => 5000,
+    'M_V' => 4000,
+    'M' => 1000,
+    'CM' => 900,
+    'D' => 500,
+    'CD' => 400,
+    'C' => 100,
+    'XC' => 90,
+    'L' => 50,
+    'XL' => 40,
+    'X' => 10,
+    'IX' => 9,
+    'V' => 5,
+    'IV' => 4,
+    'I' => 1
+  }
+
+  def romanize(number)
+    roman_result = ''
+    ROMAN_NUMBERS.each do |roman, value|
+      while number >= value
+        roman_result << roman
+        number -= value
+      end
+    end
+
+    roman_result
+  end
 
   def parse(arg)
     number = Integer(arg) rescue false
     raise RomanizerErrors::NotANumberError.new unless number.is_a? Integer
     raise RomanizerErrors::OutOfRangeError.new unless number <= 10000 && number >= 0
 
-    return number
+    return romanize(number)
   end
 end
